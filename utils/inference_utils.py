8d196f11ad4df7f14d9b7a6712b1fa396a4caf06
Henri Rebecq <h.rebecq@gmail.com>
2019-10-16 11:14:43 +0200

    Fix outer border of color reconstructions

--- utils/inference_utils.py
+++ utils/inference_utils.py
@@ -315,13 +315,13 @@ def shift_image(X, dx, dy):
     X = np.roll(X, dy, axis=0)
     X = np.roll(X, dx, axis=1)
     if dy > 0:
-        X[:dy, :] = 0
+        X[:dy, :] = np.expand_dims(X[dy, :], axis=0)
     elif dy < 0:
-        X[dy:, :] = 0
+        X[dy:, :] = np.expand_dims(X[dy, :], axis=0)
     if dx > 0:
-        X[:, :dx] = 0
+        X[:, :dx] = np.expand_dims(X[:, dx], axis=1)
     elif dx < 0:
-        X[:, dx:] = 0
+        X[:, dx:] = np.expand_dims(X[:, dx], axis=1)
     return X
 
 
