d92ab1aca123b7b0fb52a11caa4c7e7cb6a03647
Eugene Shalnov <eshalnov@google.com>
2021-12-05 16:19:55 +0300

    Fix load to work with CPU

--- utils/loading_utils.py
+++ utils/loading_utils.py
@@ -2,9 +2,9 @@ import torch
 from model.model import *
 
 
-def load_model(path_to_model):
+def load_model(path_to_model, device=None):
     print('Loading model {}...'.format(path_to_model))
-    raw_model = torch.load(path_to_model)
+    raw_model = torch.load(path_to_model, map_location=device)
     arch = raw_model['arch']
 
     try:
